a0b608595819171b299a9874cf4086b8affb95bf
aphonetic <49621437+aphonetic@users.noreply.github.com>
2020-11-22 15:02:28 -0800

    Add files via upload
    Latest changes to user.js-- new set cookie method

--- static/js/user.js
+++ static/js/user.js
@@ -97,7 +97,28 @@ let User = class {
         hiking_exp, health_assess);
 
     //JSON-ify that user and save to a cookie
-    document.cookie = 'userProfile='+JSON.stringify(sessUser);
+    setCookie("userProfile", JSON.stringify(sessUser), 30);
   };
+
   const form = document.getElementsByClassName('UserForm')[0];
   form.addEventListener('submit', handleFormSubmit);
+
+  function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
