d4e929bd3cd333a0b4d83e87faec8edc9ca2e1cf
aphonetic <49621437+aphonetic@users.noreply.github.com>
2020-11-22 15:12:54 -0800

    Update user.js
    Debugged code final

--- static/js/user.js
+++ static/js/user.js
@@ -95,30 +95,10 @@ let User = class {
     //use form elements to create a session user
     var sessUser = new User(first, last, age, height, weight, exercise_time, exercise_days,
         hiking_exp, health_assess);
-
+      
     //JSON-ify that user and save to a cookie
-    setCookie("userProfile", JSON.stringify(sessUser), 30);
+    document.cookie = 'userProfile='+JSON.stringify(sessUser);
   };
 
   const form = document.getElementsByClassName('UserForm')[0];
   form.addEventListener('submit', handleFormSubmit);
-
-  function setCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
